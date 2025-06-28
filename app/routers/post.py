9fb09420a214444d2d32a7999080e266fcea81f3
lovenesh <lovenesh73@gmail.com>
2025-06-28 15:59:50 +0530

    commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,16 +4,16 @@ from .. import models, schemas, utils
 from ..database import engine, SessionLocal, get_db
 from typing import List
 
-router=APIRouter
+router=APIRouter(prefix="/posts")
 
-@router.get("/posts", response_model=List[schemas.PostResponse])
+@router.get("/", response_model=List[schemas.PostResponse])
 def get_post(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     # cursor.execute("""SELECT * FROM posts""")
     # posts=cursor.fetchall()
     return posts
 
-@router.get("/posts/{id}", response_model=schemas.PostResponse)
+@router.get("/{id}", response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM posts WHERE id=%s """,(str(id)))
     # post=cursor.fetchone()
@@ -22,7 +22,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
     return post
     
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES(%s,%s,%s) RETURNING * """,(post.title, post.content, post.published))
     # new_post=cursor.fetchone()
@@ -33,7 +33,7 @@ def create(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.refresh(new_post)
     return new_post
 
-@router.delete("/posts/{id}")
+@router.delete("/{id}")
 def delete_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id),))
     # deleted_post=cursor.fetchone()
@@ -44,7 +44,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}")
+@router.put("/{id}")
 def put_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """,(post.title, post.content, post.published, str(id)))
     # updated_post=cursor.fetchone()
--- app/routers/user.py
+++ app/routers/user.py
@@ -3,9 +3,9 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, utils
 from ..database import engine, SessionLocal, get_db
 
-router=APIRouter
+router=APIRouter(prefix="/users")
 
-@router.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     hash_password=utils.hash(user.password)
@@ -17,7 +17,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.get("/users/{id}", response_model=schemas.UserOut)
+@router.get("/{id}", response_model=schemas.UserOut)
 def get_user(id: int,db: Session = Depends(get_db) ):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
