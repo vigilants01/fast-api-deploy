2f6f3ef7dce0bdf8aa1fecbab36bfe0845fe71e4
lovenesh <lovenesh73@gmail.com>
2025-06-28 14:43:21 +0530

    commit

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    class config:
+        orm_mode: True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    class config:
+        orm_mode: True
