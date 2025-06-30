e7b2c0e7e89dddfdcfd5514cdd9c8e352b8a2601
lovenesh <lovenesh73@gmail.com>
2025-06-30 11:09:28 +0530

    commit

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,7 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
+from sqlalchemy.orm import Session
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
@@ -23,16 +24,18 @@ def verify_access_token(token: str, credentials_exception):
         id_: str = payload.get("user_id")
         if id_ is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=id_)
+        token_data = schemas.TokenData(id=str(id_))
     except JWTError:
         raise credentials_exception
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED, 
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"}
     )
-    return verify_access_token(token, credentials_exception)
+    token=verify_access_token(token, credentials_exception)
+    user=db.query(models.User).filter(models.User.id == token.id).first()
+    return user
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oauth2
 from ..database import engine, SessionLocal, get_db
 from typing import List
-from .. import oauth2
+
 
 
 router=APIRouter(prefix="/posts", tags=['Posts'])
@@ -16,7 +16,7 @@ def get_post(db: Session = Depends(get_db)):
     return posts
 
 @router.get("/{id}", response_model=schemas.PostResponse)
-def get_post(id: int, db: Session = Depends(get_db)):
+def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id=%s """,(str(id)))
     # post=cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -25,10 +25,11 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
     
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
-def create(post: schemas.PostCreate, db: Session = Depends(get_db),  get_current_user: int = Depends(oauth2.get_current_user)):
+def create(post: schemas.PostCreate, db: Session = Depends(get_db),  current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" INSERT INTO posts (title,content,published) VALUES(%s,%s,%s) RETURNING * """,(post.title, post.content, post.published))
     # new_post=cursor.fetchone()
     # conn.commit()
+    print(current_user.email)
     new_post=models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -36,7 +37,7 @@ def create(post: schemas.PostCreate, db: Session = Depends(get_db),  get_current
     return new_post
 
 @router.delete("/{id}")
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM posts WHERE id=%s RETURNING *""",(str(id),))
     # deleted_post=cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id)
@@ -47,7 +48,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}")
-def put_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+def put_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING * """,(post.title, post.content, post.published, str(id)))
     # updated_post=cursor.fetchone()
     # conn.commit()
