33e3cdaa11962913a8368c6e6b61a5c3237c1b4d
lovenesh <lovenesh73@gmail.com>
2025-06-30 17:52:22 +0530

    commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,23 +3,30 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, utils, oauth2
 from ..database import engine, SessionLocal, get_db
 from typing import List, Optional
-
+from sqlalchemy import func
 
 
 router=APIRouter(prefix="/posts", tags=['Posts'])
 
-@router.get("/", response_model=List[schemas.PostResponse])
+@router.get("/", response_model=List[schemas.PostOut])
 def get_post(db: Session = Depends(get_db), limit: int =10, skip: int = 0, search: Optional[str] = ""):
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    #posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     # cursor.execute("""SELECT * FROM posts""")
     # posts=cursor.fetchall()
-    return posts
+    results = db.query(
+        models.Post, func.count(models.Vote.post_id).label("votes")
+    ).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True
+    ).group_by(models.Post.id).all()
+    return results
 
-@router.get("/{id}", response_model=schemas.PostResponse)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id=%s """,(str(id)))
     # post=cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True
+    ).group_by(models.Post.id).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
     return post
